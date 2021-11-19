8955e3ae99ec3867f004255bc2b659dd1260a1a6
Andrey Testov <avtestov@ya.ru>
2021-11-19 13:48:59 +0300

    Update task3

--- task3/task3.py
+++ task3/task3.py
@@ -13,14 +13,21 @@ with open(values, 'r') as file:
 val = {}
 for i in valies['values']:
     val[i['id']] = i['value']
+
+#В ТЗ не указано как формируется файл tests.json
+#Насколько я понимаю из примера получить доступ к значению можно так:
+#tests['tests'][0]['values'][1]['values'][0] ... ['values'][n]
     
 def rput(arr, val):
-    for i in arr:
-        i['value'] = val[i['id']]
-        if 'values' in i.keys():
-            i['values'] = rput(i['values'], val)
+    for elem in arr:
+        if elem['id'] in val:
+            #Проверка на случай если нет информации для id
+            elem['value'] = val[elem['id']]
+        if 'values' in elem:
+            elem['values'] = rput(elem['values'], val)
     return arr
 
 tests['tests'] = rput(tests['tests'], val)
 
-print(tests)
+with open('report.json', 'w') as file:
+    json.dump(tests, file)
